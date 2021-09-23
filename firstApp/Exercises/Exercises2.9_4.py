1dfa6614cfbc691ef3bd7a4db93539c3a644bb42
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2021-09-22 20:44:45 -0600

    Ejercicio 4

--- firstApp/Exercises/Exercises2.9_4.py
+++ firstApp/Exercises/Exercises2.9_4.py
@@ -1,10 +1,13 @@
-# Create a list of 4 float numbers, compute the average and print it
-def average():
-    my_list = [2.3, 4.5, 6.4, 8.0]
-    t = sum(my_list)
-    l = len(my_list)
-    average = t/l    
-    print("The average is {0}".format(average))
+# Create a function to convert from Celsius degrees to Fahrenheit degrees
+# Formula: F  = C * 1.8 + 32
+def askTemperature():
+    celsius: float = input("Celsius degrees: ")
+    return convert(float(celsius))
 
 
-average()
+def convert (celsius: float):
+    fahrenheit = celsius * 1.8 + 32
+    print("Fahrenheit degrees: {0}".format(fahrenheit))
+    
+
+askTemperature()
