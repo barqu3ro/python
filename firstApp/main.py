46bdbc1203063c5d723eb8a17a0b444bd8686f0a
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2021-09-22 19:19:23 -0600

    Primer ejercicio con comentarios

---
+++ firstApp/main.py
@@ -0,0 +1,9 @@
+# Ask the user name
+user_name = input("What is your name? ")
+
+# Ask the user age
+user_age = input("How old are you? ")
+
+# Print the information
+print("Hello " + user_name + ", you are " + user_age
+
