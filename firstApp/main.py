9083792714df62b778ed04be7da17f0871473398
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2021-09-22 20:01:01 -0600

    Cambio a procedimientos

--- firstApp/main.py
+++ firstApp/main.py
@@ -1,9 +1,22 @@
+def say_hello(x: str, y: str):
+    print("Hello {0}, you are {1}".format(x, y))
+    # print("TEST")
+
+
+def double_number(number: int):
+    return number * 2
+
+
+def print_double_number(number: int):
+    result = double_number(number)
+    print("Double of your age (" + str(number) + ") is " + str(result))
+
+
+# Ask user name
+user_name: str = input("What is your name? ")
+# Ask user age
+user_age: int = input("How old are you? ")
+say_hello(user_name, str(user_age))
+print_double_number(int(user_age))
+# print(double_number(user_age))
 # Ask the user name
-user_name = input("What is your name? ")
-
-# Ask the user age
-user_age = input("How old are you? ")
-
-# Print the information
-print("Hello " + user_name + ", you are " + user_age
-
