5500bc8e1f1246536e6b5160b86c3cd9defe07a1
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2023-01-30 15:44:27 -0600

    Varios

--- Pluralsight/Modularity/modularity.py
+++ Pluralsight/Modularity/modularity.py
@@ -1,16 +1,26 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+def fetch_words(url):
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf-8').split()
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
+
+def print_words(story_words):
     for word in story_words:
         print(word)
 
+
+def main(url):    
+    words = fetch_words(url)
+    print_words(words)
+
+
 if __name__ == '__main__':
-    fetch_words()
+    main(sys.argv[1])
