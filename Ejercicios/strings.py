34bfe6e98ff320b370590f68d1c9fc48f89a68c9
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2023-02-04 11:47:35 -0600

    ejercicio palindromo y longitud

--- Ejercicios/strings.py
+++ Ejercicios/strings.py
@@ -1,7 +1,17 @@
 def concatenar(first_name, last_name):
     return first_name + " " + last_name
 
+def longitud(texto):
+    return len(texto)
+
+def palindromo(texto):
+    if texto == texto[::-1]:
+        return("Es un palindromo")
+    else:
+        return("No es un palindromo")
 
 
 
-print (concatenar("jorge", "barquero"))
+cadena = input("Ingrese un texto: ")
+print ("La longitud de la cadena es: " ,longitud(cadena))
+# print (concatenar("jorge", "barquero"))
