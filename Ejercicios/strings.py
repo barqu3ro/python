fab2ed8bc8c5f91e290d3459667071beebf5e657
Jorge Barquero <78558266+barqu3ro@users.noreply.github.com>
2023-02-07 21:32:33 -0600

    mas ajustes

--- Ejercicios/strings.py
+++ Ejercicios/strings.py
@@ -4,14 +4,28 @@ def concatenar(first_name, last_name):
 def longitud(texto):
     return len(texto)
 
-def palindromo(texto):
+def is_palindromo(texto):    
     if texto == texto[::-1]:
         return("Es un palindromo")
     else:
         return("No es un palindromo")
+
+def is_palindromo_plain(text):
+    return text == text[::-1]
+
 
 
 
 cadena = input("Ingrese un texto: ")
 print ("La longitud de la cadena es: " ,longitud(cadena))
 # print (concatenar("jorge", "barquero"))
+
+# textoPalindromo = input("Ingrese un texto para verificar si es palindromo: ")
+textoPalindromo = "anita lava la tina"
+print("El texto " , textoPalindromo , is_palindromo(textoPalindromo))
+
+
+frase = "anita lava la tina"
+print(is_palindromo_plain(frase))
+
+pip ins
