09d833b6b3deed0bdf27097503b084e026fcee56
Jorge Barquero <jlbarquero21@gmail.com>
2023-02-23 22:59:31 -0600

    Ajustes en temas y diseño

--- PySimpleGui/calculator.py
+++ PySimpleGui/calculator.py
@@ -1,19 +1,22 @@
 import PySimpleGUI as sg
 
-sg.theme('LightGrey1')
-sg.set_options(font = 'Franklin 14', button_element_size=(6,3))
-
-layout =[
-    [sg.Text('output')],
-    [sg.Button('Clear'), sg.Button('Enter')],
-    [sg.Button(7),sg.Button(8),sg.Button(9),sg.Button('*')],
-    [sg.Button(4),sg.Button(5),sg.Button(6),sg.Button('/')],
-    [sg.Button(3),sg.Button(2),sg.Button(1),sg.Button('-')],
-    [sg.Button(0),sg.Button('.'),sg.Button('+')],
-    ]
+def create_window(theme):
+    sg.theme(theme)
+    sg.set_options(font = 'Franklin 14', button_element_size= (4,2))
 
 
-window = sg.Window('Calculator', layout)
+    layout =[
+        [sg.Push(),sg.Text('output', font='Franklin 30', pad=(10,20))],
+        [sg.Button('Clear', expand_x=True), sg.Button('Enter', expand_x=True)],
+        [sg.Button(7),sg.Button(8),sg.Button(9),sg.Button('*')],
+        [sg.Button(4),sg.Button(5),sg.Button(6),sg.Button('/')],
+        [sg.Button(3),sg.Button(2),sg.Button(1),sg.Button('-')],
+        [sg.Button(0, expand_x = True),sg.Button('.'),sg.Button('+')],
+        ]
+    return sg.Window('Calculator', layout)
+
+theme_menu = ['menu',['LightGrey1','dark','Darkgray8','random']]
+window = create_window('dark')
 
 while True:
     event , values = window.read()
