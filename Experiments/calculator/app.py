933c286fe881c5862b810ed585480591d4c06444
Jorge Barquero <jlbarquero21@gmail.com>
2023-06-27 11:36:58 -0600

    reorder

---
+++ Experiments/calculator/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operator = request.form['operator']
+    result = None
+
+    if operator == 'add':
+        result = num1 + num2
+    elif operator == 'subtract':
+        result = num1 - num2
+    elif operator == 'multiply':
+        result = num1 * num2
+    elif operator == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
