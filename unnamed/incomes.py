4400adfa9a069bb56ff2f8ce274a0547ac3fc4a7
Jorge Barquero <jlbarquero21@gmail.com>
2023-08-25 11:26:15 -0600

    updates in helloworld (flask coding)

--- unnamed/incomes.py
+++ unnamed/incomes.py
@@ -3,3 +3,6 @@ income_types = ["Salario", "Préstamo", "Aguinaldo", "Salario Escolar"]
 def Add_Income_Type (new_income_type):
     income_types.append(new_income_type)
 
+class Income :
+    
+    def __init__ (self, type, amount):
