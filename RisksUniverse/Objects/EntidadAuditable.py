e7a83529adc45e0f6fe3afbc6dcc7a8c0bed98b3
Jorge Barquero <jlbarquero21@gmail.com>
2023-10-18 15:52:00 -0600

    decorators

--- RisksUniverse/Objects/EntidadAuditable.py
+++ RisksUniverse/Objects/EntidadAuditable.py
@@ -1,17 +1,28 @@
-class entidad_auditalbe:
+class entidad_auditable:
     def __init__(self, id, name, description):
-        self.Id = id
-        self.Name = name
-        self.Description = description
+        self.__id = id
+        self.__name = name
+        self.__description = description
 
-    def setName(self, newName):
-        self.Name = newName
-         
-    def setDescription(self, newDescription):
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def description(self):
+        return self.__description
+
+    @name.setter
+    def name(self, newName):
+        self.__name = newName
+
+    @description.setter
+    def description(self, newDescription):
         self.Description = newDescription
 
-    def getName(self):
-        return self.Name
+
+    def name(self):
+        return self.__name
 
     def getDescription (self):
         return self.Description
