0335334221aedbcbca94d32eeb57d9fc9744c635
Jorge Barquero <jlbarquero21@gmail.com>
2023-10-20 15:08:06 -0600

    Actualización de propiedades

--- RisksUniverse/Objects/EntidadAuditable.py
+++ RisksUniverse/Objects/EntidadAuditable.py
@@ -4,26 +4,38 @@ class entidad_auditable:
         self.__name = name
         self.__description = description
 
+    # Name Property
     @property
     def name(self):
         return self.__name
-    
-    @property
-    def description(self):
-        return self.__description
 
     @name.setter
     def name(self, newName):
         self.__name = newName
 
-    @description.setter
-    def description(self, newDescription):
-        self.Description = newDescription
-
     @name.getter
     def name(self):
         return self.__name
 
+    @name.deleter
+    def name(self):
+        del self.__name
+    
+    # Description Property
+    @property
+    def description(self):
+        return self.__description
+    
+
+    @description.setter
+    def description(self, newDescription):
+        self.__description = newDescription
+    
+
     @description.getter
-    def Description (self):
-        return self.Description
+    def description (self):
+        return self.__description
+
+    @description.deleter
+    def description(self):
+        del self.__description
--- RisksUniverse/Objects/FactorRiesgo.py
+++ RisksUniverse/Objects/FactorRiesgo.py
@@ -2,33 +2,55 @@ from Objects import Riesgo
 
 class factor_riesgo:
     def __init__ (self, id, riskId, name, description):
-        self.ID = id
-        self.RiskId = riskId 
-        self.Name = name
-        self.Description  = description
+        self.__id = id
+        self.__riskId = riskId 
+        self.__name = name
+        self.__description  = description
 
-    # Getters
-    def getName(self):
-        return self.Name
+    # Name Property
+    @property
+    def name (self):
+        return self.__name
 
-    def getRiskId(self):
-        return self.RiskId
+    @name.getter
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(newName):
+        self.__name = newName
+
+    # RiskID Property
+    @property
+    def riskId (self):        
+        return self.__riskId
+
+    @riskId.getter
+    def riskId (self):
+        return self.__riskId
+
+    @riskId.setter
+    def riskId (self, newRiskId):
+        self.__riskId = newRiskId
+
+    @riskId.deleter
+    def riskId (self):
+        del self.__riskId
     
-    def getDescription (self):
-        return self.Description
-    
-
-    # Setters
-    def setName(self, newName):
-        self.Name = newName
-
-    def setDescription (self , newDescription):
-        self.Description = newDescription
-
-    def setRiskId (self, newRiskId):
-        self.RiskId = newRiskId
-
-
-
+    # Description Property
+    @property
+    def description (self):
+        return self.__description
 
     
+    @description.getter
+    def description (self):
+        return self.__description
+
+    @description.setter
+    def description (self, newDescription):
+        self.__description = newDescription
+
+    @description.deleter
+    def description (self):
+        del self.__description
